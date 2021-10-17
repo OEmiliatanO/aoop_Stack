b511842c7b0ded88eaa9c48ca86129136555450e
OEmiliatanO <47825105+OEmiliatanO@users.noreply.github.com>
2021-10-17 13:24:24 +0800

    Add files via upload

--- c2/stack.c
+++ c2/stack.c
@@ -2,76 +2,9 @@
 #include <stdio.h>
 #include "stack.h"
 
-/*
-void push(struct stack *this, int x)
-{
-    if (this->size == 0)
-    {
-        this->now = (struct stacknode *)malloc(sizeof(struct stacknode));
-        if (this->now == NULL)
-        {
-            return;
-        }
-        ++this->size;
-        this->now->last = this->now->nex = NULL;
-        this->now->val = x;
-        return;
-    }
-
-    this->now->nex = (struct stacknode *)malloc(sizeof(struct stacknode));
-    if (this->now->nex == NULL)
-    {
-        return;
-    }
-    ++this->size;
-    this->now->nex->last = this->now;
-    this->now = this->now->nex;
-    this->now->nex = NULL;
-    this->now->val = x;
-
-    return;
-}
-
-int pop(struct stack *this)
-{
-    if (this->size == 0) return 0;
-
-    int res = this->now->val;
-    if (this->now->last)
-    {
-        this->now = this->now->last;
-        free(this->now->nex);
-        --this->size;
-        this->now->nex = NULL;
-
-        return res;
-    }
-
-    free(this->now);
-    --this->size;
-    this->now = NULL;
-
-    return res;
-}
-
-struct stack* new_stack()
-{
-    struct stack* p = (struct stack*)malloc(sizeof(struct stack));
-    p->size = 0;
-    p->now = NULL;
-    return p;
-}
-
-void delete_stack(struct stack* this)
-{
-    while(this->size) pop(this);
-    free(this);
-    this = NULL;
-}
-*/
 stack* new_stack()
 {
-    stack *tmp = calloc(1, sizeof(stack));
+    stack *tmp = malloc(sizeof(stack));
     if (tmp == NULL) return NULL;
 
     tmp->ptr = tmp->size_ptr = tmp->now_size_ptr = 0;
@@ -82,7 +15,7 @@ stack* new_stack()
     {
         if (!nodesize_dym) return NULL;
 
-        tmp->arr[0] = calloc(nodesize_dym, sizeof(int));
+        tmp->arr[0] = malloc(nodesize_dym * sizeof(int));
         tmp->size[0] = nodesize_dym;
         nodesize_dym >>= 1;
 
@@ -104,12 +37,13 @@ void push(stack *this, int val)
         {
             if (!nodesize_dym) return;
 
-            this->arr[this->size_ptr] = calloc(nodesize_dym, sizeof(int));
+            this->arr[this->size_ptr] = malloc(nodesize_dym * sizeof(int));
             this->size[this->size_ptr] = nodesize_dym;
             nodesize_dym >>= 1;
 
         }while(this->arr[this->size_ptr] == NULL);
 
+        this->size[this->size_ptr] = 0;
         this->size[this->size_ptr] += this->size[this->size_ptr - 1];
         this->now_size_ptr = this->size_ptr;
         this->arr[this->size_ptr][this->ptr - this->size[this->size_ptr - 1]] = val;
