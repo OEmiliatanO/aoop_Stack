cd49ec402b7d9c4ad3a1120c17ebc4644e98744d
OEmiliatanO <47825105+OEmiliatanO@users.noreply.github.com>
2021-10-22 21:57:45 +0800

    Update stack.c
    add comments about the conversion of the pointer on virtual array to the position on real array

--- c2/stack.c
+++ c2/stack.c
@@ -65,12 +65,19 @@ void push(stack *this, int val)
 
         this->size[this->size_ptr] += this->size[this->size_ptr - 1];                // maintain the prefix sum of size of arrays
         this->now_size_ptr = this->size_ptr;                                         // move pointer of size array to maximum position
+        
+        // since the stack only know where's the pointer on a "big virtual" array, which's combined by lots of array,
+        // the pointer must be fixed to the real array which we use right now.
+        // and because this->size[] maintain the prefix sum of size of arrays,
+        // we can simply use "this->ptr - this->size[this->size_ptr - 1]"
+        // to determine what's the position going to write on this "real" array
         this->arr[this->size_ptr][this->ptr - this->size[this->size_ptr - 1]] = val; // push value
     }
     else
     {
         while (this->size[this->now_size_ptr] - 1 < this->ptr)
             ++this->now_size_ptr;
+        // this one is same to line 74, but add some protection to prevent over crossing the array
         this->arr[this->now_size_ptr][this->ptr - (this->now_size_ptr == 0 ? 0 : this->size[this->now_size_ptr - 1])] = val;
     }
 }
