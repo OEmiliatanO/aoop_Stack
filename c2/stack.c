1f7cf2ae6c526aa889a1308ae93d148efd557ab6
25077667 <zxc25077667@protonmail.com>
2021-10-22 10:33:47 +0800

    Add advice in comment lines
    The '!' at the begin of the comment means it is emergency to fix.
    The '?' at the begin of the comment means it a discussion.

--- c2/stack.c
+++ c2/stack.c
@@ -1,18 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "stack.h"
+// ! Line 68, 74, and 85 are a little bit obfuscation, can you give more comments or graphics?
 
 // stack structure contains pointer array where puts data,
 // size[] maintains the prefix sum of array size,
 // size_ptr represents the maximum position on pointer array that can be used,
 // now_size_ptr represents what the position the pointer is now on,
-// ptr represents a pointer to a virtual array which's combinded by many arraies.
+// ptr represents a pointer to a virtual array which's combinded by many arrays.
 
 // return a pointer to stack structure
-stack* new_stack()
+stack *new_stack()
 {
+    // ? Use `tmp` as `new_stk` might be better
     stack *tmp = malloc(sizeof(stack));
-    if (tmp == NULL) return NULL;
+    if (tmp == NULL)
+        return NULL;
 
     tmp->ptr = tmp->size_ptr = tmp->now_size_ptr = 0;
 
@@ -21,14 +24,15 @@ stack* new_stack()
     // require memory. if failed, require smaller
     do
     {
-        if (!nodesize_dym) return NULL;
+        if (!nodesize_dym)
+            return NULL; //! FIXME: THe tmp is leakage
 
         tmp->arr[0] = malloc(nodesize_dym * sizeof(int));
         tmp->size[0] = nodesize_dym;
         nodesize_dym >>= 1;
 
-    }while(tmp->arr[0] == NULL);
-    
+    } while (tmp->arr[0] == NULL);
+
     return tmp;
 }
 
@@ -36,44 +40,51 @@ stack* new_stack()
 void push(stack *this, int val)
 {
     ++this->ptr;
-    
+
     // judge if cross the boundary of the total array space
     if (this->size[this->size_ptr] - 1 < this->ptr)
     {
         ++this->size_ptr;
-        if (this->size_ptr >= _MAXN_) return;
+        if (this->size_ptr >= _MAXN_)
+            return;
 
-        // require memory. if failed, require smaller
-        int nodesize_dym = _NODEESIZE_;
-        do
-        {
-            if (!nodesize_dym) return;
+        { // ! Duplicated code as line [24, 34]
+            // require memory. if failed, require smaller
+            int nodesize_dym = _NODEESIZE_;
+            do
+            {
+                if (!nodesize_dym)
+                    return;
 
-            this->arr[this->size_ptr] = malloc(nodesize_dym * sizeof(int));
-            this->size[this->size_ptr] = nodesize_dym;
-            nodesize_dym >>= 1;
+                this->arr[this->size_ptr] = malloc(nodesize_dym * sizeof(int));
+                this->size[this->size_ptr] = nodesize_dym;
+                nodesize_dym >>= 1;
 
-        }while(this->arr[this->size_ptr] == NULL);
+            } while (this->arr[this->size_ptr] == NULL);
+        }
 
-        this->size[this->size_ptr] += this->size[this->size_ptr - 1]; // maintain the prefix sum of size of arraies 
-        this->now_size_ptr = this->size_ptr; // move pointer of size array to maximum position
+        this->size[this->size_ptr] += this->size[this->size_ptr - 1];                // maintain the prefix sum of size of arrays
+        this->now_size_ptr = this->size_ptr;                                         // move pointer of size array to maximum position
         this->arr[this->size_ptr][this->ptr - this->size[this->size_ptr - 1]] = val; // push value
     }
     else
     {
-        while(this->size[this->now_size_ptr] - 1 < this->ptr) ++this->now_size_ptr;
+        while (this->size[this->now_size_ptr] - 1 < this->ptr)
+            ++this->now_size_ptr;
         this->arr[this->now_size_ptr][this->ptr - (this->now_size_ptr == 0 ? 0 : this->size[this->now_size_ptr - 1])] = val;
     }
 }
 
 int pop(stack *this)
 {
+    // ? Might not have to consider this case.
     // invalid
-    if (this->ptr == 0) return -1;
+    if (this->ptr == 0)
+        return -1;
 
     int res = this->arr[this->now_size_ptr][this->ptr - (this->now_size_ptr == 0 ? 0 : this->size[this->now_size_ptr - 1])];
     --this->ptr;
-    
+
     // if pointer cross the left boundary of now array
     if (this->now_size_ptr > 0 && this->ptr - this->size[this->now_size_ptr - 1] < 0)
         --this->now_size_ptr;
